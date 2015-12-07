24ecf9b4099039044be058f90197c6369a7dc1d2
danpotty <danpotmail@yahoo.com>
2015-12-07 11:15:59 -0800

    login functions controller/factory, home html

--- public/javascript/controllers/GlobalController.js
+++ public/javascript/controllers/GlobalController.js
@@ -1,11 +1,24 @@
 (function() {
   'use strict';
   angular.module('app')
-  .controller('GlobalController', GlobalController);
+    .controller('GlobalController', GlobalController);
 
   function GlobalController() {
     var vm = this;
 
+    vm.register = function() {
+      UserFactory.register(vm.user).then(function(res) {
+        $state.go('Home');
+      });
+    };
+
+    vm.login = function() {
+      UserFactory.login(vm.user).then(function(res) {
+        $state.go('Home');
+      });
+    };
+
+    vm.logout = UserFactory.removeToken;
 
   }
 })();
--- public/javascript/services/UserFactory.js
+++ public/javascript/services/UserFactory.js
@@ -1,10 +1,53 @@
 (function() {
   'use strict';
   angular.module('app')
-  .factory('UserFactory', UserFactory);
+    .factory('UserFactory', UserFactory);
 
   function UserFactory($http, $q) {
     var o = {};
+
+    o.register = function(user) {
+      var q = $q.defer();
+      $http.post('/api/v1/users/register', user).then(function(res) {
+        o.setToken(res.data.token);
+        q.resolve(res.data);
+      });
+      return q.promise;
+    };
+
+    o.login = function(user) {
+      var q = $q.defer();
+      $http.post('/api/v1/users/login', user).then(function(res) {
+        o.setToken(res.data.token);
+        q.resolve(res.data);
+      });
+      return q.promise;
+    };
+
+    o.getToken = function() {
+      return $window.localStorage.getItem('token');
+    };
+
+    o.setToken = function(token) {
+      $window.localStorage.setItem('token', token);
+      o.setUser();
+    };
+
+    o.removeToken = function() {
+      $window.localStorage.removeItem('token');
+      o.status._id = null;
+      o.status.email = null;
+      o.status.username = null;
+    };
+
+    o.setUser = function() {
+      var token = JSON.parse(atob(o.getToken().split('.')[1]));
+      o.status._id = token._id;
+      o.status.email = token.email;
+      o.status.username = token.username;
+    };
+
+    if (o.getToken()) o.setUser();
 
     return o;
   }
