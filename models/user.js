03e402848f2191ed4da04bce2ceb888f4aa511fa
jlprager <jaclyn.prager@gmail.com>
2015-12-07 10:37:02 -0800

    security/hashing added to userSchema

--- models/user.js
+++ models/user.js
@@ -19,4 +19,31 @@ let UserSchema = new mongoose.Schema({
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment"}]
 });
 
+UserSchema.methods.CreateHash = function(password, cb){
+  let SALT_ROUNDS = 10;
+  if(process.env.NODE_ENV === 'test') SALT_ROUNDS = 1;
+  bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
+    if(err) return cb(err);
+    bcrypt.hash(password, salt, (err, hash) => {
+      if(err) return cb(err);
+      cb(null, hash);
+    });
+  });
+};
+
+UserSchema.methods.validatePassword = function(password, hash, cb){
+  bcrypt.compare(password, hash, (err, res) => {
+    if(err) return cb(err);
+    cb(null, res);
+  });
+};
+
+UserSchema.methods.generateJWT = function(){
+  return jwt.sign({
+    _id: this._id,
+    email: this.local.email,
+    name: this.name
+  }, 'hiskett & sons');
+};
+
 module.exports = mongoose.model('User', UserSchema);
