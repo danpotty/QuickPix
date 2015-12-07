7b1a8a37543f3805ce1983b0db796e14727f3554
danpotty <danpotmail@yahoo.com>
2015-12-07 11:24:29 -0800

    server

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
 let port = process.env.PORT || 3000;
+let mongoose = require('mongoose');
+let passport = require('passport');
+require('./models/user');
+require('./models/post');
+require('./models/comment');
+require('./config/passport');
+if(process.env.NODE_ENV === 'test') mongoose.connect('mongodb://localhost/quickpix-test')
+else mongoose.connect('mongodb://localhost/quickpix')
+
 
 app.set('views', './views');
 app.engine('.html', require('ejs').renderFile);
@@ -15,9 +24,21 @@ app.set('view options', {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+
+let userRoutes = require('./routes/userRoutes');
+let postRoutes = require('./routes/postRoutes');
 
 app.get('/', function(req, res) {
 	res.render('index');
+});
+
+app.use('/api/v1/users/', userRoutes);
+app.use('/api/v1/posts/', postRoutes);
+
+app.use((err, req, res, next) => {
+	if(process.env.NODE_ENV !== 'test') {console.log(err)}
+	res.status(500).send(err);
 });
 
 module.exports = app.listen(port, () => {
